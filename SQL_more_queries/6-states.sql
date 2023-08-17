f21d999496d22066e736bcb0bee0ede7ba4db357
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-17 19:09:46 +0300

    states table

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,10 +1,10 @@
 -- creates  a database and the table states
 -- creates a database hbtn_0d_usa
-CREATE DATABASE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- creates a table in database
 USE hbtn_0d_usa;
 
-CREATE TABLE states
+CREATE TABLE IF NOT EXISTS states
 (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(256)
