bbb56c6d0d40331ed4419b55ea379a9c69543266
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-17 19:02:24 +0300

    cities by states

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all cities in the database
-SELECT cities.id, cities.name, states.name
+SELECT DISTINCT cities.id, cities.name, states.name
 FROM cities INNER JOIN states
 ORDER BY cities.id;
