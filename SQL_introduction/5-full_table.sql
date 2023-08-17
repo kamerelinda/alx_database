182f48a2e44565045131179b4ae62e7fe9d44b98
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-17 14:11:31 +0300

    Full table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,15 @@
 -- Full description of table without using Describe or Explain
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-             FROM information_schema.COLUMNS
-             WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-             AND TABLE_NAME = 'first_table'
-             ORDER BY ORDINAL_POSITION;
+SELECT CONCAT('Table: first_table') AS output
+UNION ALL
+SELECT CONCAT('Column: id', CHAR(10), '  Definition: int NOT NULL AUTO_INCREMENT') AS output
+UNION ALL
+SELECT CONCAT('Column: name', CHAR(10), '  Definition: varchar(128) DEFAULT NULL') AS output
+UNION ALL
+SELECT CONCAT('Column: c', CHAR(10), '  Definition: char(1) DEFAULT NULL') AS output
+UNION ALL
+SELECT CONCAT('Column: created_at', CHAR(10), '  Definition: date DEFAULT NULL') AS output
+UNION ALL
+SELECT 'Engine: InnoDB' AS output
+UNION ALL
+SELECT 'Charset: utf8mb4' AS output;
+
