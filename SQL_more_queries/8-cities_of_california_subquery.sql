62d1779c07946d2768baca42531f42d146aac93d
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-17 17:36:33 +0300

    Cities of carlifornia

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- lists all cities of california that can be found in database
-SELECT cities.id, name
-FROM cities, states
-WHERE name='Carlifornia'
-ORDER BY cities.id ASC ;
+SELECT id, name
+FROM cities
+WHERE (SELECT DISTINCT name='California')
+ORDER BY cities.id;
