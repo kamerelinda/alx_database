8e58d54df17acda56c029bab719b95f93200683e
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-17 19:46:47 +0300

    cities table

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -6,7 +6,7 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities
 (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    name VARCHAR(256) NOT NULL,
     state_id INT NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id)
+    FOREIGN KEY (state_id) REFERENCES states(id),
+    name VARCHAR(256) NOT NULL
 );
