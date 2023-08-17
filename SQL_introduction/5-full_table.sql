35180f0684f71298a74de6a99c920dcad095e51f
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-08-17 16:13:27 +0300

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,15 +1,2 @@
 -- Full description of table without using Describe or Explain
-SELECT CONCAT('Table: first_table') AS output
-UNION ALL
-SELECT CONCAT('Column: id', CHAR(10), '  Definition: int NOT NULL AUTO_INCREMENT') AS output
-UNION ALL
-SELECT CONCAT('Column: name', CHAR(10), '  Definition: varchar(128) DEFAULT NULL') AS output
-UNION ALL
-SELECT CONCAT('Column: c', CHAR(10), '  Definition: char(1) DEFAULT NULL') AS output
-UNION ALL
-SELECT CONCAT('Column: created_at', CHAR(10), '  Definition: date DEFAULT NULL') AS output
-UNION ALL
-SELECT 'Engine: InnoDB' AS output
-UNION ALL
-SELECT 'Charset: utf8mb4' AS output;
-
+SHOW TABLE STATUS
